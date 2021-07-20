c77a778d8d40507336ba4b67d564c50ecc3df37e
Surya R <87648143+Surya-SR@users.noreply.github.com>
2021-07-20 13:05:10 +0530

    added the java file

---
+++ Array.java
@@ -0,0 +1,61 @@
+package com.company;
+
+public class Array {
+    private int[] items;
+    private int count;
+
+    public Array(int length) {
+        items = new int[length];
+
+    }
+
+    public void insert(int item) {
+        //if the array is full ,resize it
+        if (items.length == count) {
+            //create a new array (twice the size)
+            int[] newItems = new int[count * 2];
+            //copy all the existing items
+            for (int i = 0; i < count; i++) {
+                newItems[i] = items[i];
+
+            }
+            //set "items "to the this new array
+            items = newItems;
+
+
+        }
+        items[count++] = item;
+
+
+    }
+
+    public void removeAt(int index) {
+        //validate the index
+        if (index < 0 || index >= count)
+            throw new IllegalArgumentException();
+        //shift the items to the left to fill the hole
+        for (int i = index; i < count; i++) {
+            items[i] = items[i + 1];
+        }
+        count--;
+
+    }
+
+
+    public void print() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(items[i]);
+        }
+    }
+
+    public int indexof(int item) {
+        for (int i = 0; i < count; i++) {
+            if (items[i] == item) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+
+}
